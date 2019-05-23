3c9cae9b69d809c7855d477d23f6993f13573ffb
Diego Torres <diegotc86@gmail.com>
2019-05-23 11:21:01 -0500

    Refactor listChannels function

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -13,7 +13,41 @@ function addChannel(name, author) {
   if (!app.channels.some(channel => channel.name == name)) {
     let newChannel = createNewChannel(name, author);
     app.channels.push(newChannel);
-    return newChannel;
+    return true;
   }
   return false;
 }
+
+function listChannels() {
+  let elements = "";
+  app.channels.forEach(channel => {
+    elements += `<li>${channel.name}</li>`;
+  });
+  let channelList = document.getElementById("channels_list");
+  channelList.innerHTML = elements;
+}
+
+var $formAddChannel = document.getElementById("addChannelForm");
+
+$formAddChannel.addEventListener("submit", handleAddChannelSubmit);
+
+function handleAddChannelSubmit(event) {
+  event.preventDefault();
+  let $error = document.getElementById("channelError");
+  $error.innerText = "";
+  let $newChannelName = event.target.elements.newChannelName;
+  let user = {
+    id: app.currentuser.id,
+    username: app.currentuser.username
+  };
+  if (addChannel($newChannelName.value, user)) {
+    listChannels();
+    $newChannelName.value = "";
+  } else {
+    $error.innerText = "This channel already exists";
+  }
+}
+
+window.onload = () => {
+  listChannels();
+};
