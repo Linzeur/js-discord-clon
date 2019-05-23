03f707812fe01597585a3d3d4e1a6d8509a29ab3
Diego Torres <diegotc86@gmail.com>
2019-05-23 12:00:39 -0500

    List channels on index page (#24)
    * Refactor listChannels function
    
    * Move  variable to window.onload function
    
    * Fix conflicts
    
    * Modify HTML to add UL, form and span for addChannel function

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,3 +1,5 @@
+var $formAddChannel;
+
 function createNewChannel(name, author) {
   return {
     id: Date.now(),
@@ -13,7 +15,39 @@ function addChannel(name, author) {
   if (!app.channels.some(channel => channel.name == name)) {
     let newChannel = createNewChannel(name, author);
     app.channels.push(newChannel);
-    return newChannel;
+    return true;
   }
   return false;
 }
+
+function listChannels() {
+  let elements = "";
+  app.channels.forEach(channel => {
+    elements += `<li>${channel.name}</li>`;
+  });
+  let channelList = document.getElementById("channels_list");
+  channelList.innerHTML = elements;
+}
+
+function handleAddChannelSubmit(event) {
+  event.preventDefault();
+  let $error = document.getElementById("channelError");
+  $error.innerText = "";
+  let $newChannelName = event.target.elements.newChannelName;
+  let user = {
+    id: app.currentuser.id,
+    username: app.currentuser.username
+  };
+  if (addChannel($newChannelName.value, user)) {
+    listChannels();
+    $newChannelName.value = "";
+  } else {
+    $error.innerText = "This channel already exists";
+  }
+}
+
+window.onload = function() {
+  $formAddChannel = document.getElementById("addChannelForm");
+  $formAddChannel.addEventListener("submit", handleAddChannelSubmit);
+  listChannels();
+};
