d192e5f3b2f8c79cc1201fd9be8bf9aeb9736291
Paulo Tijero <paulotijero@gmail.com>
2019-05-25 19:29:29 -0500

    Add scrollbar (#68)
    * Add event active
    
    * Add scrollbar

---
+++ public/assets/js/action.js
@@ -0,0 +1,6 @@
+function showMemberList() {
+  let div = document.getElementById("divMembers");
+  let svg = document.getElementById("btnPeople");
+  div.classList.toggle("state-members");
+  svg.classList.toggle("svg-active");
+}
